e2c0ea00f5f7d89b87f314f67c45365a253eff23
BrianBok <brian.bokusmc@gmail.com>
2020-09-29 17:39:20 -0400

    Set flexbox for main div

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+// Toggle Sidebar
+function toggleMenu(){
+    var sidebar = document.querySelector('.sidebar');
+    var toggle = document.querySelector('.toggle');
+    sidebar.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
