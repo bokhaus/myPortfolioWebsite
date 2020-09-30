c5ded42735f8a0d9a9efa15cb4e0810511ac4bed
BrianBok <brian.bokusmc@gmail.com>
2020-09-30 00:44:34 -0400

    Added type.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+
 // Toggle Sidebar
 function toggleMenu(){
     var sidebar = document.querySelector('.sidebar');
@@ -6,3 +7,17 @@ function toggleMenu(){
     toggle.classList.toggle('active');
 }
 
+// Type.JS
+if ($(".text-slider").length == 1) { 
+              
+    var typed_strings =  
+        $(".text-slider-items").text(); 
+
+    var typed = new Typed(".text-slider", { 
+        strings: typed_strings.split(", "), 
+        typeSpeed: 70, 
+        loop: true, 
+        backDelay: 900, 
+        backSpeed: 30, 
+    }); 
+} 
