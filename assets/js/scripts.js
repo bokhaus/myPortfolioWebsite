7a5e8917618b1b7c838a41951073cd2214eb0e51
BrianBok <brian.bokusmc@gmail.com>
2020-10-04 13:38:28 -0400

    added project folders and created a back to top button.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,3 +22,24 @@ if ($(".text-slider").length == 1) {
         backSpeed: 30, 
     }); 
 } 
+
+// Scroll to top button
+//Get the button
+var mybutton = document.getElementById("toTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
